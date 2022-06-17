94fd920c1e9b7d7c1a8e04ce71f4d6eac9673207
Kwasi Dankwa <edankwa14@gmail.com>
2022-06-17 19:33:18 -0400

    still fixing errors

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require("fs");
 
 //Server
 const app = express();
-const PORT = process.env || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
